33abc0f87e044bd641a393f0109e70cb3f594f27
Marcel Losso Forte <marcel.losso@gmail.com>
2019-12-24 05:49:05 -0300

    Fixed navbar top and added animation in

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,23 @@ $('.nav a').on('click', function(e) {
         }, 700);
     }
 })
+
+//Fix navbar on top
+var num = 1080;
+
+$(window).bind('scroll', function() {
+    if($(window).scrollTop() < num){
+        $('.nav').removeClass('fixed');
+        $('.nav').removeClass('animateIn');
+        $('.nav').addClass('animateOut');
+        $('.navbarC').addClass('container');
+        console.log('oi');
+    } else {
+        $('.navbarC').removeClass('container');
+        $('.nav').addClass('fixed');
+        $('.nav').removeClass('animateOut');
+        $('.nav').addClass('animateIn');
+        console.log('oi2');
+    }
+})
+

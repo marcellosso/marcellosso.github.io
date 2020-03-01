999f0633c7e8624b949ff1cd3b4c31c9bc702ffd
Marcel <marcel.losso@gmail.com>
2020-03-01 19:50:50 -0300

    Added contact form back-end

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,44 @@ $(window).bind('scroll', function() {
     }
 })
 
+// var message = "";
+// var form = document.querySelector('#contactForm'),
+//     name = form.querySelector('[name="name"]');
+
+// $("#sendMessage").on("click", function(e) {
+//     e.preventDefault();
+//     console.log("foi");
+//     console.log(name.value);
+
+//     $("#sendMessage").value = 'Sending...';
+//     message = $("#contactForm").serialize();
+//     console.log(message);
+
+//     var xhr = new XMLHttpRequest();
+
+//     xhr.open('POST', '//formspree.io/marcel.losso@gmail.com', true);
+//     xhr.setRequestHeader("Accept", "application/json");
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//     xhr.onloadend = function (res) {
+//         if(res.target.status === 200){
+//             console.log("Sucesso");
+//         } else {
+//             console.log("erro");
+//             console.log(res.target.status);
+//         }
+//     }
+
+//     xhr.send("name=" + message);
+//     // $.ajax({
+//     //     url: "//formspree.io/xayjdnod",
+//     //     method: "POST",
+//     //     data: {message: message},
+//     //     dataType: "json",
+//     //     success: function(data) {
+//     //         console.log("SUCESSO -> " + data)
+//     //     }
+//     // });
+//     // alert('Thanks');
+//     // return false;
+// })
